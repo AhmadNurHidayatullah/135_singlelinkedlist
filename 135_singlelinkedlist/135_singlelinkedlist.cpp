e648fffaca71f3b68c03807bc3fe4582b80498b5
AhmadNurHidayatullah <127074146+AhmadNurHidayatullah@users.noreply.github.com>
2023-05-18 13:06:59 +0700

    semua fungsi void addnode

--- 135_singlelinkedlist/135_singlelinkedlist.cpp
+++ 135_singlelinkedlist/135_singlelinkedlist.cpp
@@ -32,7 +32,22 @@ void addNode() {
         return;
     }
 
-    
+    Node* previous = START;
+    Node* current = START;
+
+    while ((current != NULL) && (nim >= current->noMhs))
+    {
+        if (nim == current->noMhs)
+        {
+            cout << "NIM sudah ada" << endl;
+            return;
+        }
+        previous = current;
+        current = current->next;
+    }
+
+    nodebaru->next = current;
+    previous->next = nodebaru;
 }
 int main()
 {
