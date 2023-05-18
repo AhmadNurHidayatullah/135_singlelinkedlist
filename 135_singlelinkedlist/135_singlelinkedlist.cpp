bbc4d2c8fa6a93c1111dec527ffeef0d7dbf6b3e
AhmadNurHidayatullah <127074146+AhmadNurHidayatullah@users.noreply.github.com>
2023-05-18 13:26:30 +0700

    menambahkan fungsi void traverse

--- 135_singlelinkedlist/135_singlelinkedlist.cpp
+++ 135_singlelinkedlist/135_singlelinkedlist.cpp
@@ -91,7 +91,19 @@ bool listempty() {
 }
 
 void traverse() {
-
+    if (listempty()) {
+        cout << "List kosong" << endl;
+        system("pause");
+        system("cls");
+        return;
+    }
+    else {
+        Node* currentNode = START;
+        while (currentNode != NULL) {
+            cout << "NIM: " << currentNode->noMhs << ", Nama: " << currentNode->name << endl;
+            currentNode = currentNode->next;
+        }
+    }
 }
 int main()
 {
